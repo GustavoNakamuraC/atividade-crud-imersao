9d2ac04557efdba9a0c8e13cb574498daa3f7c95
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-04-08 19:32:13 -0300

    build/Criando estrutura do backend

---
+++ src/main/java/com/atividade/CrudRpg/CrudRpgApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.CrudRpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRpgApplication.class, args);
+	}
+
+}
