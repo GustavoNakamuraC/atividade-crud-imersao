0b360a78e3444d30dce755519c22891a74d3ac91
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-04-09 16:55:42 -0300

    feat/Finalizando PersonagemMapper

--- src/main/java/com/atividade/CrudRpg/mapper/ItemMagicoMapper.java
+++ src/main/java/com/atividade/CrudRpg/mapper/ItemMagicoMapper.java
@@ -2,11 +2,18 @@ package com.atividade.CrudRpg.mapper;
 
 import com.atividade.CrudRpg.controller.dto.ItemMagicoDto;
 import com.atividade.CrudRpg.domain.ItemMagico;
+import com.atividade.CrudRpg.repository.entity.ItemMagicoEntity;
 
 public class ItemMagicoMapper {
     public static ItemMagico dtoParaDomain(ItemMagicoDto item) {
     }
 
     public static ItemMagicoDto domainParaDto(ItemMagico item) {
+    }
+
+    public static ItemMagico entityParaDomain(ItemMagicoEntity itemMagicoEntity) {
+    }
+
+    public static ItemMagicoEntity domainParaEntity(ItemMagico itemMagico) {
     }
 }
--- src/main/java/com/atividade/CrudRpg/mapper/PersonagemMapper.java
+++ src/main/java/com/atividade/CrudRpg/mapper/PersonagemMapper.java
@@ -3,6 +3,7 @@ package com.atividade.CrudRpg.mapper;
 import com.atividade.CrudRpg.controller.dto.PersonagemDto;
 import com.atividade.CrudRpg.domain.ItemMagico;
 import com.atividade.CrudRpg.domain.Personagem;
+import com.atividade.CrudRpg.repository.entity.PersonagemEntity;
 
 import java.util.List;
 
@@ -33,6 +34,36 @@ public class PersonagemMapper {
                         .map(ItemMagicoMapper::domainParaDto)
                         .toList())
                 .forca(domain.getForca())
+                .defesa(domain.getDefesa())
+                .build();
+    }
+
+    public static Personagem entityParaDomain(PersonagemEntity entity){
+        return Personagem.builder()
+                .id(entity.getId())
+                .nome(entity.getNome())
+                .nomeFantasia(entity.getNomeFantasia())
+                .classe(entity.getClasse())
+                .level(entity.getLevel())
+                .itensMagicos(entity.getItensMagicos().stream()
+                        .map(ItemMagicoMapper::entityParaDomain)
+                        .toList())
+                .forca(entity.getDefesa())
+                .defesa(entity.getDefesa())
+                .build();
+    }
+
+    public static PersonagemEntity domainParaEntity(Personagem domain){
+        return PersonagemEntity.builder()
+                .id(domain.getId())
+                .nome(domain.getNome())
+                .nomeFantasia(domain.getNomeFantasia())
+                .classe(domain.getClasse())
+                .level(domain.getLevel())
+                .itensMagicos(domain.getItensMagicos().stream()
+                        .map(ItemMagicoMapper::domainParaEntity)
+                        .toList())
+                .forca(domain.getDefesa())
                 .defesa(domain.getDefesa())
                 .build();
     }
